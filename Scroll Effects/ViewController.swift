c86b9f69718a39783012b899701ea29067d08cd9
saadetsimsek <saadetsimsek13@gmail.com>
2024-07-03 15:17:55 +0300

    Initial Commit

---
+++ Scroll Effects/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scroll Effects
+//
+//  Created by Saadet Şimşek on 03/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
